b55486ecf84e0ede6a93ba1b6958c50423a095a5
Pieter Robyns <pieter.robyns@outlook.com>
2016-04-07 17:03:13 +0200

    Added OTAA example

---
+++ src/example_rn2483_join_otaa.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import configparser
+from rpyutils import printd, Level, Color, clr, set_debug_level
+from loranode import RN2483Controller
+
+# LoRaController OTAA based join and message test
+if __name__ == "__main__":
+    set_debug_level(Level.DEBUG)
+
+    # Parse config
+    cp = configparser.ConfigParser()
+    cp.read('lora.cfg')
+    config = cp['DEFAULT']
+
+    # Test controller
+    lc = RN2483Controller(config.get("port"))
+    if lc.test():
+        printd("[+] Connected to LoRa RN2483 device", Level.INFO)
+    else:
+        printd(clr(Color.YELLOW, "[-] Failed to get version from LoRa device"), Level.WARNING)
+
+    # Join and send a message
+    if lc.join_otaa(config.get("appkey"), config.get("appeui"), config.get("deveui")):
+        printd("[+] Connected to gateway", Level.INFO)
+        lc.set_adr(True)
+        lc.set_pwr(14)
+        if lc.send("AC", ack=False):
+            printd(clr(Color.GREEN, "[+] Test succeeded"), Level.CRITICAL)
+            del lc
+            exit()
+
+    printd(clr(Color.RED, "[-] Test failed"), Level.CRITICAL)
